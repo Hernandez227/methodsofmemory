142c9f6239790e5bcf692c2ded84ec301294b631
Hernandez227 <59767622+Hernandez227@users.noreply.github.com>
2020-06-20 21:19:10 -0500

    first commit

---
+++ malloc.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+//Since the size of int is 4 bytes, this statement will allocate 400 bytes 
+//of memory. And, the pointer ptr holds the address of the first byte in the allocated memory.
+int main() 
+{ 
+
+	// This pointer will hold the 
+	// base address of the block created 
+	int* ptr; 
+	int n, i; 
+
+	// Get the number of elements for the array 
+	n = 5; 
+	printf("Enter number of elements: %d\n", n); 
+	
+
+	// Dynamically allocate memory using malloc() 
+	ptr = (int*)malloc(n * sizeof(int)); 
+
+	// Check if the memory has been successfully 
+	// allocated by malloc or not 
+	if (ptr == NULL) { 
+		printf("Memory not allocated.\n"); 
+		exit(0); 
+	} 
+	else { 
+
+		// Memory has been successfully allocated 
+		printf("Memory successfully allocated using malloc.\n"); 
+
+		// Get the elements of the array 
+		for (i = 0; i < n; ++i) { 
+			ptr[i] = i + 1; 
+		} 
+
+		// Print the elements of the array 
+		printf("The elements of the array are: "); 
+		for (i = 0; i < n; ++i) { 
+			printf("%d, ", ptr[i]); 
+		} 
+	} 
+
+	return 0; 
+} 
+
